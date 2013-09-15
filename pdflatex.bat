de3bec62aeec27f0e0376eeafce804108a3b21b7
rodrigo.g.branco@gmail.com <rodrigo.g.branco@gmail.com@99411ed3-b83a-c963-bda6-ef4cb3c331e9>
2013-09-15 22:18:13 +0000

    Lista de siglas atualizada.

--- pdflatex.bat
+++ pdflatex.bat
@@ -1,6 +1,4 @@
-del /s tmp/*.* /q
-del /s *.aux *.acn *.acr *.alg *.glg *.glo *.gls *.ist *.lof *.log *.out *.toc /q
 pdflatex.exe %1
-bibtex.exe %1
 makeglossaries %~n1
+bibtex.exe %1
 pdflatex.exe %1
